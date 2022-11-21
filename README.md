93adf2f92c2dc362a0ce87fbf107f9cd2070981e
CharlesPizza <85193781+CharlesPizza@users.noreply.github.com>
2022-11-20 19:06:15 -0500

    Added clientGUI first frame

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
 # Chat Application
 Here we use sockets to build a basic chat application. The application is comprised of a server side and client side, both of which have Graphical User Interfaces.
-
+We use the following librarys: socket, threading, queue, tkinter
 ## Server
 The serverGUI.py provides a basic 3x4 grid interface to take in a given IP address and ports. IP address is auto-detected, but changeable in the GUI. Then, the server will attempt to launch and bind the provided address and port and listen for connections.
 The server is configured to accept up to 16 connections, and prompts all new connections for a nickname. The server will accept messages up to 1024 bytes. The server uses the broadcast() method to send the message back out to all connected clients.<br>
 ![Alt text](Images/Server%20Launch.png "Server Launch GUI")<br>
-
+> The application automatically detects the external IP of the device it is being run on.
 
 ## Client
-The client GUI is comprised of three screens, the first screen is the client launcher, which takes in an address and port to connect and bind to.
-
-The upon successful connection the first screen is replaced with a second screen, which serves primarily for a connection confirmation, and asks for a username.
+The client GUI is comprised of three screens, the first screen is the client launcher, which takes in an address and port to connect and bind to. It also asks for a Username which will be passed to the server after connection.<br>
+![Alt text](Images/Client%20Launch.png "Client Launch Frame")<br>
 
 Once a username is provided the final screen is comprised of two section, a chatbox where all messages are displayed, and below that a input box where the user can type out their next message.
-This frame will accept both clicking the button to submit, and pressing the enter button to submit a message.
+
+This frame will accept both clicking the button to submit, and pressing the enter button to submit a message.<br>
+![Alt text](Images/Client%20Chat%20Window.png "Client Chat Frame")
--- clientGUI.py
+++ clientGUI.py
@@ -1,23 +1,66 @@
 from tkinter import *
 from tkinter import ttk 
 import socket
+import client
 
+def get_ip():
+    hostname = socket.gethostname()
+    ip = socket.gethostbyname(hostname)
+    return ip
 
+width = 250
+height = 125
 root = Tk()
 root.title('Client Launch')
-
-mainframe = ttk.Frame(root)
-mainframe.grid(column=0, row=0)
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
-input_frame = ttk.Frame(root)
-input_frame.grid(column=0, row=1)
-#====================================ChatBox====================================
-scrollbar = Scrollbar(mainframe)
-scrollbar.pack(side=RIGHT, fill=Y)
+root.geometry(f'{width}x{height}')
+launch_frame = ttk.Frame(root)
+launch_frame.grid(column=0, row=0)
+#==================================== Row 1 ====================================
+# IP Address /
+# ----------/
+ttk.Label(launch_frame, text='IP Address:').grid(column=0, row=0, sticky=W)
+ip_addr=ttk.Entry(launch_frame,)
+ip_addr.grid(column=1, row=0, padx=10, pady=(5,0), sticky=(W,S))
+ip_addr.insert(0, get_ip())
 
-#====================================SendMSG====================================
-input_box = ttk.Entry(input_frame, width=100)
-input_box.grid(column=0, row=1, padx=10,pady=10)
+#==================================== Row 2 ==================================== 
+# Port /
+# ----/
+ttk.Label(launch_frame,text='External Port:').grid(column=0, row=1, sticky=W)
+eport=ttk.Entry(launch_frame,)
+eport.grid(column=1, row=1,padx=10, pady=5,sticky=(W))
+eport.insert(0, 9000)
+
+#==================================== Row 3 ====================================
+# Get Username  /
+# -------------/
+ttk.Label(launch_frame,text='Username:').grid(column=0, row=2, sticky=W)
+username=ttk.Entry(launch_frame,)
+username.grid(column=1, row=2,padx=10, pady=0,sticky=(W))
+username.insert(0,str(socket.gethostname()))
+
+#==================================== Row 4 ====================================
+# Launch Server /
+# -------------/
+launch_btn = ttk.Button(launch_frame,text="Launch Server", command=client.connect)
+launch_btn.grid(column=0, row=3, columnspan=2,padx=5, pady=5, sticky=(W,E))
+
+
+
+if not launch_frame:
+    mainframe = ttk.Frame(root)
+    mainframe.grid(column=0, row=0)
+    input_frame = ttk.Frame(root)
+    input_frame.grid(column=0, row=1)
+    #====================================ChatBox====================================
+    scrollbar = Scrollbar(mainframe)
+    scrollbar.pack(side=RIGHT, fill=Y)
+
+    #====================================SendMSG====================================
+    input_box = ttk.Entry(input_frame, width=100)
+    input_box.grid(column=0, row=1, padx=10,pady=10)
+
 
 root.mainloop()
